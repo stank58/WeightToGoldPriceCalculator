9bd9590542e1b9db14e1ca344270740d437eac27
Stanners <55584387+stank58@users.noreply.github.com>
2025-01-14 16:18:18 +0000

    Add files via upload

--- Weight to Gold Price Calculator/WeightToGold.py
+++ Weight to Gold Price Calculator/WeightToGold.py
@@ -1,7 +1,12 @@
 import requests
-
+import locale
 from datetime import datetime
-now = datetime.today().isoformat()
+
+locale.setlocale(locale.LC_ALL, 'en_GB')
+
+now = datetime.today().strftime('%d-%m-%Y')
+def format_currency(amount):
+    return locale.currency(amount, grouping=True)
 
 api_url = "https://data-asg.goldprice.org/dbXRates/GBP"
 
@@ -11,20 +16,45 @@ headers = {
 
 data = requests.get(api_url, headers=headers).json()
 
-weight = int(input("What is your weight? "))
-gold_ounce = data["items"][0]["xauPrice"]
-unit = input("Is that (L)b or (K)g?")
-gold_conversion = gold_ounce * 35.274
+print("Weight To Gold Calculator")
 
-if unit == "K" or unit == "k":
-    your_worth = gold_conversion * weight
-    print("If you were made of gold, you would weigh £" + str(your_worth) + " as of " + str(now))
-elif unit == "L" or unit == "l":
-    converted_weight = weight / 2.20462
-    your_worth = gold_conversion * converted_weight
-    print("If you were made of gold, you would weigh £" + str(your_worth) + " as of " + str(now))
-else:
-    print("Error you fucked up")
-
+while True:
+    while True:
+        try:
+            weight = float(input("What is your weight?: "))
+            break  # Exit the loop if input is valid
+        except ValueError:
+            print("Error: Please enter just a number.")
     
-input('Press ENTER to exit')
+    gold_ounce = data["items"][0]["xauPrice"] #pulls the data from the api for the price at the beginning of the morning.
+    newdate = data["date"]
+    gold_conversion = gold_ounce * 32.1507 #this is the calculation as the API pulls in ounces and the weight we want is in KG.
+
+    while True:
+        unit = input("Is that (L)b or (K)g?")
+        if unit == "K" or unit == "k":
+            your_worth = gold_conversion * weight
+            total = format_currency(your_worth)
+            gold_total = format_currency(gold_conversion)
+            print("If you were made entirely of gold, you would weigh " + str(total) + " based on the gold price of " + str(gold_total) + " per kg as of " + str(newdate))
+            break
+        elif unit == "L" or unit == "l":
+            converted_weight = weight / 2.20462
+            your_worth = gold_conversion * converted_weight
+            total = format_currency(your_worth)
+            gold_total = format_currency(gold_conversion)
+            print("If you were made entirely of gold, you would weigh " + str(total) + " based on the gold price of " + str(gold_total) + " per kg as of " + str(newdate))
+            break
+        else:
+            print("Error: Please enter only L or K.")
+
+    again = input("Thank you again for using this calculator, would you like to try again? (y or n)")
+
+    while(again != "n") and (again != "y"):
+        again = input("Please type a valid response. Would you like to try again? Please type y for yes or n for no-")
+
+    if again == "n" :
+        input("(Press Enter to Exit)")
+        break
+    elif again == "y" :
+        continue
